66c8e200b4c9cf99757c52e4beb6b58645952316
Vivek Kumar <www.vivekkumar0412@gmail.com>
2022-05-12 01:33:56 +0530

    Few programms are added

---
+++ 1.cpp
@@ -0,0 +1,71 @@
+  // #include<bits/stdc++.h>
+  // using namespace std;
+  // int main(){
+  //     // char a;
+  //     // cout<<"ENTER THE CHARACTER : "<<endl;
+  //     // cin>>a;
+  //     // cout<<"THE ASCII VALUE OF "<<a<<" IS "<<(int)a<<endl;
+
+  //     // long int a= 100000;
+  //     // long int c= 100000;
+  //     // long int b = a*c;
+  //     // cout<<b<<endl;
+  //     // cout<<10^4<<endl;
+
+  //     long long int l;
+  //     long long int b;
+  //     cout<<"ENTER THE VALUE OF LENGTH : "<<endl;
+  //     cin>>l;
+  //     cout<<"ENTER THE VALUE OF BREADTH : "<<endl;
+  //     cin>>b;
+  //     long long int area = l*b;
+  //     cout<<"AREA OF RECTANGLE IS : "<<area<<endl;
+      
+
+  //     return 0;
+  // }
+
+  // #include<bits/stdc++.h>
+  // using namespace std;
+  // int main(){
+  //   int a = 'a';
+  //   cout<<a<<endl;
+  //   return 0;
+  // }
+
+// $$$$$$$$$$$$$$$$ BITWISE OPERATORS $$$$$$$$$$$$$$$$
+  // #include<bits/stdc++.h>
+  // using namespace std;
+  // int main(){
+  //   int a = 4;
+  //   int b = 6;
+  //   cout<<"a&b is : "<<(a&b)<<endl;
+  //   cout<<"a|b is : "<<(a|b)<<endl;
+  //   cout<<"a~b is : "<<~a<<endl;
+  //   cout<<"a^b is : "<<(a^b)<<endl;
+
+  //   // $$$$$$$$$$$$$$$$ LEFT SHIFT AND RIGHT SHIFT OPERATOR $$$$$$$$$$$$$$$$
+  //   cout<<(17<<1)<<endl;
+  //   cout<<(17>>2)<<endl;
+  //   cout<<(21<<2)<<endl;
+  //   cout<<(16>>3)<<endl;
+  //   return 0;
+  // }
+
+  // $$$$$$$$$$$$$$$$ SUM OF N NUMBERS $$$$$$$$$$$$$$$$
+  // #include<bits/stdc++.h>
+  // using namespace std;
+  // int main(){
+  //   int n;
+  //   int sum = 0;
+  //   cout<<"ENTER THE VALUE OF N: "<<endl;
+  //   cin>>n;
+  //   for(int i = 0; i<=n; i++){
+  //     sum = sum + i;
+  //   cout<<"total is : "<<sum<<endl;
+  //   };
+  //   return 0;
+  // }
+
+  // $$$$$$$$$$$$$$$$ FEBONNACHI SERIES $$$$$$$$$$$$$$$$
+  
